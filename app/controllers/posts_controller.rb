ffa65960f210bb5f92e1cc253874d1b6e01ed6d8
Vincent <caojunvincent@gmail.com>
2014-07-10 08:04:03 +0800

    finish post index and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,25 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.order("id DESC")
+  end
+
+  def show
+    @post = Post.find params[:id]
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 PostitTemplate::Application.routes.draw do
+  get "posts/index"
+  get "posts/show"
   root to: 'posts#index'
+
+  resources :posts, except: [:destroy]
 end
